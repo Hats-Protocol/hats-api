ae325504ad10017296d423cce65d91e8feb15c24
Ido <gershido@gmail.com>
2024-06-29 13:51:09 +0300

    wip

--- src/redis.ts
+++ src/redis.ts
@@ -13,6 +13,65 @@ export class RedisCacheClient {
     });
   }
 
+  async invalidateEntity(entityName: string, entityId: string): Promise<void> {
+    const entity = `${entityName}.${entityId}`;
+    const matchParam = `*${entity}*`;
+    const stream = this._client.scanStream({
+      match: matchParam,
+    });
+
+    const keysToDelete: string[] = [];
+    const operations: Promise<any>[] = [];
+
+    await new Promise((resolve, reject) => {
+      stream.on("data", (resultKeys: string[]) => {
+        for (let key of resultKeys) {
+          key = key.slice(15);
+          let hash: string | undefined = undefined;
+          if (key.startsWith(entity)) {
+            hash = key.slice(entity.length + 1);
+          } else if (key.endsWith(entity)) {
+            hash = key.slice(0, key.length - entity.length - 1);
+          }
+
+          if (hash !== undefined && !keysToDelete.includes(hash)) {
+            keysToDelete.push(hash);
+            const delPromise = this._client.del(`response-cache:${hash}`);
+            operations.push(delPromise);
+          }
+        }
+      });
+
+      stream.on("end", resolve);
+      stream.on("error", reject);
+    });
+
+    Promise.all(operations)
+      .then(() => {
+        log.info(
+          `Invalidation: ${JSON.stringify({
+            name: entityName,
+            id: entityId,
+            hashes: keysToDelete,
+          })}`
+        );
+      })
+      .catch((error) => {
+        log.error(
+          `Invalidation Error: ${JSON.stringify({
+            name: entityName,
+            id: entityId,
+            hashes: keysToDelete,
+            error: error,
+          })}`
+        );
+        throw new Error(
+          `Error invalidating entity ${entityName} with ID ${entityId}: ${error}`
+        );
+      });
+  }
+
+  /*
   deleteMappingEntities(entryId: string) {
     const matchParam = `*${entryId}*`;
     const stream = this._client.scanStream({
@@ -59,4 +118,5 @@ export class RedisCacheClient {
       log.info(`Invalidated entity ${entityName} with ID ${entityId}`);
     });
   }
+    */
 }
