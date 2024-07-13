be940a5825e712ef5f0a57f249a15a3de063d1b7
Ido <gershido@gmail.com>
2024-07-14 00:07:07 +0300

    wip

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import express from "express";
 import cors from "cors";
 import "dotenv/config";
-import { createBuiltMeshHTTPHandler } from "../.mesh";
+import { createBuiltMeshHTTPHandler } from "../.mesh/index";
 import { CacheInvalidationManager } from "./invalidation";
 import logger from "./log";
 import {
